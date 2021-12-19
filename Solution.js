2e378a591a07e2a191c601f42cc8fb9d28097053
Ankit Kumar Singh <72161660+ankit-singh706@users.noreply.github.com>
2021-12-20 00:10:29 +0530

    Update Solution.js

--- Solution.js
+++ Solution.js
@@ -1,144 +1,71 @@
-class FixEquation {
-    findMissingDigit(S) {  // String Parameter
-        const Arr = S.split(" ");
-        var flag = 0
-        var missingDigPos = 1;
-        var pos = 0;
-        var res = 0;
-        var ans = -1;
-        for(let x in Arr){
-            if (x == "+" || x == "-" || x == "*" || x == "*"){
-                missingDigPos += 1;
-                continue;
-            }
-            else{
-                for(let y in x){
-                    if(y=="?"){
-                        flag = 1;
-                        break;
-                    }
-                    pos++;
-                }
-                if(flag==1){
-                    break;
-                }
-            }
-
-        }
-        var A = parseInt(Arr[0]);
-        var B = parseInt(Arr[2]);
-        var C = parseInt(Arr[4]);
-        var D = parseInt(Arr[6]);
-        flag = 0;
-        switch (missingDigPos){
-            case 1:
-                var B = parseInt(Arr[2]);
-                var C = parseInt(Arr[4]);
-                var D = parseInt(Arr[6]);
-                res = parseInt( (D - C) / B );
-                const Digits = [];
-                var i = 0;
-                var temp = res;
-                while(temp){
-                    Digits[i++] = temp%10;
-                    temp/=10;
-                }
-                i--;
-                for (x in Arr[0]){
-                    if(Digits[i--]==parseInt(x[0])){
-                        flag = 1; 
-                    }
-                    else if(x == "?"){
-                        ans = Digits[i--];
-                    }
-                    else{
-                        flag = 0;
-                        break;
-                    }
-                }
-                break;
-            case 2:
-                var A = parseInt(Arr[0]);
-                var C = parseInt(Arr[4]);
-                var D = parseInt(Arr[6]);
-                res = parseInt( (D - C) / A );
-                const Digits = [];
-                var i = 0;
-                var temp = res;
-                while(temp){
-                    Digits[i++] = temp%10;
-                    temp/=10;
-                }
-                i--;
-                for (x in Arr[2]){
-                    if(Digits[i--]==parseInt(x[0])){
-                        flag = 1; 
-                    }
-                    else if(x == "?"){
-                        ans = Digits[i--];
-                    }
-                    else{
-                        flag = 0;
-                        break;
-                    }
-                }
-                break;
-            case 3:
-                var A = parseInt(Arr[0]);
-                var B = parseInt(Arr[2]);
-                var D = parseInt(Arr[6]);
-                res = parseInt( D - (A*B) );
-                const Digits = [];
-                var i = 0;
-                var temp = res;
-                while(temp){
-                    Digits[i++] = temp%10;
-                    temp/=10;
-                }
-                i--;
-                for (x in Arr[4]){
-                    if(Digits[i--]==parseInt(x[0])){
-                        flag = 1; 
-                    }
-                    else if(x == "?"){
-                        ans = Digits[i--];
-                    }
-                    else{
-                        flag = 0;
-                        break;
-                    }
-                }
-                break;
-            case 4:
-                var A = parseInt(Arr[0]);
-                var B = parseInt(Arr[2]);
-                var C = parseInt(Arr[4]);
-                res = parseInt( (A*B) + C );
-                const Digits = [];
-                var i = 0;
-                var temp = res;
-                while(temp){
-                    Digits[i++] = temp%10;
-                    temp/=10;
-                }
-                i--;
-                for (x in Arr[6]){
-                    if(Digits[i--]==parseInt(x[0])){
-                        flag = 1; 
-                    }
-                    else if(x == "?"){
-                        ans = Digits[i--];
-                    }
-                    else{
-                        flag = 0;
-                        break;
-                    }
-                }
-                break;
-        }
-        if (flag == 0){
-            return -1;
-        }
-        return ans;
+class solution{
+  string eq;
+  solution(string eq)
+  {
+    this.eq=eq;
+  }
+  var solve()
+  {
+    const num=eq.split(" ");
+    if(num[0].search("?")!=-1)
+    {
+      var B=parseInt(num[2]);
+      var C=parseInt(num[4]);
+      var D=parseInt(num[6]);
+      if(B==0 || (D-C)%B==0)
+      return -1;
+      var O=tostring((D-C)/B);
+      for(int i=0;i<num[0].size();i++)
+      {
+        if(num[0][i]=='?')
+        return O[i];
+      }
     }
+    else if(num[2].search("?")!=-1)
+    {
+      var A=parseInt(num[0]);
+      var C=parseInt(num[4]);
+      var D=parseInt(num[6]);
+      if(A==0 || (D-C)%A==0)
+      return -1;
+      var O=tostring((D-C)/A);
+      for(int i=0;i<num[2].size();i++)
+      {
+        if(num[2][i]=='?')
+        return O[i];
+      }
+    }
+    else if(num[4].search("?")!=-1)
+    {
+      var A=parseInt(num[0]);
+      var B=parseInt(num[2]);
+      var D=parseInt(num[6]);
+      var O=tostring(D-A*B);
+      for(int i=0;i<num[4].size();i++)
+      {
+        if(num[4][i]=='?')
+        return O[i];
+      }
+    }
+    else if(num[6].search("?")!=-1)
+    {
+      var A=parseInt(num[0]);
+      var B=parseInt(num[2]);
+      var C=parseInt(num[4]);
+      var O=tostring(A*B+C);
+      for(int i=0;i<num[6].size();i++)
+      {
+        if(num[6][i]=='?')
+        return O[i];
+      }
+    }
+    else 
+    return -1;
+  }
 }
+
+var eq=prompt("Enter the equation");
+
+solution s=new solution(eq);
+
+console.log(s.solve());
